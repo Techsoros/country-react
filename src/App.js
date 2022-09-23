d00177be9af86c2b286be80a2e393353cded061c
Your Name~ <f.zihad333@gmail.com>
2022-09-23 20:26:49 +0600

    Country ApI Loading complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useEffect, useState } from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1>Visit the World</h1>
+      <LoadAPI></LoadAPI>
+    </div>
+  );
+}
+
+function LoadAPI() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div className="c">
+      <h2>
+        Available Country ::
+        <span style={{ color: "blue" }}> {countries.length}</span>
+      </h2>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            name={country.name.common}
+            area={country.area}
+            capital={country.capital ? country.capital[0] : "not available"}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
+}
+function Country(props) {
+  return (
+    <div className="country-card">
+      <h3>{props.name}</h3>
+      <p>Capital city : {props.capital}</p>
+      <p>Tota Area={props.area} Square KM</p>
     </div>
   );
 }
