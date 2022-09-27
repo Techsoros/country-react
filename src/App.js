04935d97d20c3cf768fe5cf899ac58d24aa3a182
Your Name~ <f.zihad333@gmail.com>
2022-09-27 22:53:02 +0600

    Clean my HTML

--- src/App.js
+++ src/App.js
@@ -4,37 +4,9 @@ import Todos from "./components/Todos/Todos";
 function App() {
   return (
     <div className="App">
-      <Todos></Todos>
+      <h2>Clean my JSX</h2>
     </div>
   );
 }
-
-// function Blog(props) {
-//   const [love, setLove] = useState(0);
-
-//   const giveLove = () => setLove(love + 1);
-//   const removeLove = () => {
-//     if (love > 0) {
-//       setLove(love - 1);
-//     }
-//   };
-
-//   return (
-//     <div className="blog">
-//       <h2 className="title" style={headerStyle}>
-//         {props.title}
-//       </h2>
-//       <p>{props.news}</p>
-//       <p>💘 : {love}</p>
-//       <div
-//         className="button-group"
-//         style={{ display: "flex", justifyContent: "space-around" }}
-//       >
-//         <button onMouseMove={giveLove}>Send Love💘</button>
-//         <button onMouseMove={removeLove}>Remove Love💘</button>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default App;
