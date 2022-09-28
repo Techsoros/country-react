e16aa09281db9022c21d159546f7b4ed085415bf
Your Name~ <f.zihad333@gmail.com>
2022-09-28 19:08:43 +0600

    Clean my react space

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import Todos from "./components/Todos/Todos";
 
 function App() {
   return (
     <div className="App">
-      <h2>Clean my JSX</h2>
+      <h2>Mewo</h2>
     </div>
   );
 }
